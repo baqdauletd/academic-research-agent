367866cb8e88dfed894a3d3389d4227240fb11f3
baqdauletd <baqdaulet.dauletov04@gmail.com>
2026-07-18 20:23:44 +0500

    changed log calls due to changes in logging package

--- src/app/cli.py
+++ src/app/cli.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from monitoring.logger import log_query_generation
+from monitoring.research import log_query_generation
 from tools.query import generate_queries
 
 prompt = input("Enter your prompt: ")
@@ -10,8 +10,9 @@ generated_queries: list[str] = []
 try:
     generated_queries = generate_queries(prompt)
     log_query_generation(run_id, prompt, generated_queries)
+    print(generated_queries)
 except Exception as e:
-    log_query_generation(run_id, prompt, generated_queries, err=str(e))
+    log_query_generation(run_id, prompt, generated_queries, error=str(e))
     raise
 
 #starting, query generation, call API, normalization, deduplication, storing in psql, finishing
